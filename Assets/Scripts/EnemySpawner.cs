8cbb1d38000d12d2644e41d2056a9a89e853d7e3
VGDC-lab <notpresident35@gmail.com>
2023-01-18 11:44:05 -0800

    Add enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public AnimationCurve SpawnCooldownOverTime;
+
+    private float spawnCooldown = 0;
+
+    void Update()
+    {
+        if (spawnCooldown <= 0) {
+            // Hard-coded position, could be changed
+            Instantiate(EnemyPrefab, new Vector3 (Random.Range (-6f, 6f), 8, 0), Quaternion.identity, null);
+            spawnCooldown = SpawnCooldownOverTime.Evaluate(Time.time);
+        }
+
+        spawnCooldown -= Time.deltaTime;
+    }
+}
